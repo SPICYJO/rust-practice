2488d0da4026abf885c7059ec93e93a32580aba3
Seungwoo Jo <sjosjo1204@gmail.com>
2020-06-21 12:09:20 +0900

    Finish Chapter 2

--- 1_Getting_Started/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Jo\rust-practice\1_Getting_Started\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\Jo\rust-practice\1_Getting_Started\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
