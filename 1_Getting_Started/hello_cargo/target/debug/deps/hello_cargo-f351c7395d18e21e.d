fa79f4365cf4b5f2b99344340c8bf325c778ed1b
Seungwoo Jo <sjosjo1204@gmail.com>
2020-06-21 10:49:54 +0900

    Finish Chapter 1

---
+++ 1_Getting_Started/hello_cargo/target/debug/deps/hello_cargo-f351c7395d18e21e.d
@@ -0,0 +1,5 @@
+C:\Users\Jo\rust-practice\1_Getting_Started\hello_cargo\target\debug\deps\hello_cargo-f351c7395d18e21e.rmeta: src\main.rs
+
+C:\Users\Jo\rust-practice\1_Getting_Started\hello_cargo\target\debug\deps\hello_cargo-f351c7395d18e21e.d: src\main.rs
+
+src\main.rs:
---
+++ 1_Getting_Started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Jo\rust-practice\1_Getting_Started\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Jo\rust-practice\1_Getting_Started\hello_cargo\src\main.rs
